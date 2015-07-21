8654934fe6866a005024f6efbe24650e386b2770
OOTANI,Kenji <ootani@virgintech.co.jp>
2015-07-21 19:40:44 +0900

    〜#28ステージ

--- MoveBlock01.m
+++ MoveBlock01.m
@@ -14,6 +14,7 @@
 float amplitude;//振幅
 float period;//周期
 int normZero;//0リセット
+float velocity;
 
 - (void)didLoadFromCCB
 {
@@ -25,10 +26,22 @@ int normZero;//0リセット
         amplitude=13;
         period=30*2;//周期30 → Cnt:188==0
         normZero=188*2;
+        velocity=0.2;
     }else if([GameManager getCurrentStage]==18){
         amplitude=5;
         period=30*2;//周期30 → Cnt:188==0
         normZero=188*2;
+        velocity=0.2;
+    }else if([GameManager getCurrentStage]==26){
+        amplitude=5;
+        period=30*2;//周期30 → Cnt:188==0
+        normZero=188*2;
+        velocity=0.2;
+    }else if([GameManager getCurrentStage]==28){
+        amplitude=2.5;
+        period=30*2;//周期30 → Cnt:188==0
+        normZero=188*2;
+        velocity=0.5;
     }
     
     [self schedule:@selector(move_Schedule:) interval:0.016 repeat:CCTimerRepeatForever delay:delay];
@@ -44,7 +57,7 @@ int normZero;//0リセット
     //調整用
     //NSLog(@"Cnt=%d Y=%f",moveCnt,y);
     
-    self.position=ccp(self.position.x,self.position.y + y*0.2);
+    self.position=ccp(self.position.x,self.position.y + (y*velocity));
     
     if(moveCnt % normZero==0){
         moveCnt=0;
